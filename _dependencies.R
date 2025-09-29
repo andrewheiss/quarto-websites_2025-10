dfc361f40058039e66abb0d85d30fa6a901a9ea7
Andrew Heiss <andrew@andrewheiss.com>
2025-09-29 13:55:44 -0400

    Built site for gh-pages

--- _dependencies.R
+++
@@ -1,6 +0,0 @@
-# Things that need to be installed but didn't get installed automatically as 
-# dependencies because {renv} couldn't see them
-library(quarto)
-library(collapse)
-library(yaml)
-library(litedown)
